63c5a1ad5e005a440f4ab33d18c8b0066d4b0006
Streetfighter4 <yassen4o222@abv.bg>
2017-01-16 15:02:07 +0200

    make index in elasticsearch

--- src/dynamic/home.py
+++ src/dynamic/home.py
@@ -3,8 +3,9 @@ import traceback
 from flask import Flask, request, make_response, jsonify, render_template
 app = Flask(__name__)
 
-#from elasticsearch import Elasticsearch
-#es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}], timeout=30)
+es.cluster.health(wait_for_status='yellow', request_timeout=5)
 
 import requests
 import json
@@ -15,34 +16,27 @@ def tracking():
     print (request.get_json())
     print('hey2')
     jsonDict = request.get_json()
+    print(jsonDict)
     issueUrl = jsonDict['issue']['self']
-    print (issueUrl)
-
-    r = requests.get(issueUrl, auth=('yasen.alexiev@abv.bg', 'Streetfighter4'))
-    print(r.json()['fields']['summary'])
-
-    #     data = request.data
-    #     try:
-    #         jdata = json.load(data)
-    #
-    #         if jdata['webhookEvent'] == 'jira::issue_created':
-    #
-    #
-    #     except:
-    #         print
-    #         traceback.format_exc()
-    #         msg = 'Incorrect data! Can not parse to json format'
-    #         print
-    #         msg
-    #         return make_response(msg, 500)
+    print(issueUrl)
 
 
-
-   # r = requests.get('http://localhost:9200')
+    r = requests.get('http://localhost:9200')
     i = 1
-    # while i < 10:
-    #     es.index(index='devgauge', doc_type='jiraData', id=i, body=json.loads(r.text))
-    #     i = i + 1
+    if r.status_code == 200:
+        r = requests.get(issueUrl , auth=('yasen.alexiev@abv.bg', 'Streetfighter4'))
+        #print(r.json()['fields']['summary'])
+
+
+        es.index(index='dev_gauge', doc_type='jira_issues', id=r.json()['id'], body=r.json())
+        res = es.get(index='dev_gauge', doc_type='jira_issues', id=r.json()['id'])
+        print(res)
+        print(res['_source'])
+        res = es.search(index='dev_gauge', body={"query": {"match_all": {}}})
+        print("Got %d Hits:" % res['hits']['total'])
+        for hit in res['hits']['hits']:
+            print("%(summary)s" % hit["_source"]['fields'])
+
     return render_template('index.html')
 
 if __name__ == "__main__":
